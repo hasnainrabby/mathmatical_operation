33625042037872ec85ddfdeeba4a6bd5f082207e
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2019-08-09 00:48:55 +0600

    Initial commit

---
+++ mathematical_operation.py
@@ -0,0 +1,24 @@
+number1 = 10
+print(number1)
+number2 = 25
+print(number2)
+number3 = 50
+print(number3)
+number4 = 35
+print(number4)
+
+#//addition
+result = number1 + number2
+print(result)
+
+#//subtraction
+result = number3 - number4
+print(result)
+
+#//multiplication
+result = number3 * number2
+print(result)
+
+#//divition
+result = number2 / number1
+print(result)
